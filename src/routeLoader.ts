233d84bb98f7c95469e4689d9d09489e9b9ea68e
Dhevan Muhamad <dhevanmuhamad@gmail.com>
2021-07-16 13:05:54 +0700

    some fix

--- src/routeLoader.ts
+++ src/routeLoader.ts
@@ -3,11 +3,12 @@ import 'reflect-metadata';
 import AuthController from './modules/core/controller/auth.controller';
 import isAuthenticated from './modules/core/middlewares/auth.middleware';
 import ManagementRouter from './modules/management/management.router';
-import masterDataRouter from './modules/masterData/masterData.controller';
+import { RootRouter } from './router/router';
+
 const RouteLoader = (app: express.Application) => {
   app.use('/auth', AuthController);
   app.use('/management', isAuthenticated, ManagementRouter);
-  app.use('/master-data', isAuthenticated, masterDataRouter)
+  app.use(RootRouter)
 };
 
 export = RouteLoader;
